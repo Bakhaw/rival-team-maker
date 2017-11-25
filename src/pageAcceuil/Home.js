26dcf4f497cba585b1423eee9c9ebc1298685e58
prescise <prescillia.seck@gmx.fr>
2017-11-25 21:03:17 +0100

    ajout tout

---
+++ src/pageAcceuil/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component{
+  render(){
+    return(
+      <div className="Home">
+        <div className="textAccueil">
+          <h1 className="acc">Construisez votre communauté</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
