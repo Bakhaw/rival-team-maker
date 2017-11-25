160bd651a6f1023423608f3cd395443a2d946368
Faïssal <faissal.hattou@gmail.com>
2017-11-25 15:21:47 +0100

    Commiting for merge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import ubisoftLogo from './logo_ubisoft.png';
+import { Checkbox } from 'react-bootstrap';
 import './App.css';
 
+const items = [
+  '#id: 20218498371',
+  '#Plateforme: PS4',
+  '#Jeux: Just Dance, The Crew',
+];
+
 class App extends Component {
+
+  createCheckboxes = () => {
+    items.map(this.createCheckbox);
+  };
+
+  createCheckbox = label => {
+    <Checkbox label={label} handleCheckboxChange={this.toggleCheckbox} key={label} />;
+  };
+
+  toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ubisoftLogo} className="App-logo" alt="logo ubisoft" />
-          <h1>Hackathon</h1>
-          <h1>PIMP THE DATA !</h1>
-          <h1 className="App-title">UBISOFT X SIMPLON</h1>
-          <p className="App-intro">#RivalMaker</p>
-        </header>
+      <div className='container-fluid'>
+        <h3>Rechercher un joueur</h3>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.createCheckboxes()}
+          <button className='btn btn-default' type='submit'>Rechercher</button>
+        </form>
       </div>
     );
   }
